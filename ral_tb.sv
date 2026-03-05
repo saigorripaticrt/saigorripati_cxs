df4f15f775a58bf6183c3f85dbcb83039c523a95
saigorripaticrt <158164581+saigorripaticrt@users.noreply.github.com>
2026-03-05 09:44:01 +0530

    Update ral_tb.sv

--- ral_tb.sv
+++ ral_tb.sv
@@ -1,13 +1,18 @@
-// ---------------------------- understanding set, get, get_mirrored_value, & update methods ------------------------------
+ 
+// --------------------------------------  explicit predictor  --------------------------------------
+ 
  
  
  
 `include "uvm_macros.svh"					// will give an access to uvm macros
 import uvm_pkg::*;							// will give an access to uvm pkg(to all uvm classes)
-///////////////////////transaction class
+ 
+ 
+// ------------------- transaction class ---------------------
  
  
 class transaction extends uvm_sequence_item;
+  `uvm_object_utils(transaction)
  
   	 bit[7:0] din;
 	 bit      wr_enb;
@@ -18,23 +23,12 @@ class transaction extends uvm_sequence_item;
 	 bit[7:0] dout;
 
  
- 
-	`uvm_object_utils_begin(transaction)			// factory registration and field macros
-		`uvm_field_int(din, UVM_ALL_ON   +  UVM_DEC)  	// | or + UVM_DEC decimal format flag
-		`uvm_field_int(wr_enb, UVM_ALL_ON  + UVM_DEC)	// UVM_DEFAULT or UVM_ALL_ON flag
-		`uvm_field_int(addr, UVM_ALL_ON  +  UVM_DEC)
-		`uvm_field_int(dout, UVM_ALL_ON  +  UVM_DEC)	
-	`uvm_object_utils_end					// if re-ordering these then printing will happen in another way 
-								// and impact on pack and unpack methods
   	function new(input string name = "transaction");
     		super.new(name);
   	endfunction: new
 endclass: transaction
  
  
-// IMP:- 
-// --- we are using register sequence hence not adding bus sequence ----
-//  direct driver class
  
 
 // ------------------- driver class ---------------------
@@ -54,9 +48,9 @@ endclass: transaction
 // during the simulution.
  
  
-class driver extends uvm_driver#(transaction);
+class driver extends uvm_driver #(transaction);
  
-	transaction req;
+	
 	virtual dut_interface vif;
 
  
@@ -76,7 +70,7 @@ class driver extends uvm_driver#(transaction);
 		req = transaction::type_id::create("req");					// req is an inbuild object of xtn type
 		if(!uvm_config_db #(virtual dut_interface)::get(this, "", "virtual_intf", vif))	// hence no need to declare it
 		`uvm_fatal(get_type_name(), "Unable to get virtual_intf vif")			// get can be return a bit type value 0 -> if failed 
-	endfunction: build_phase								// 1 -> passed
+	endfunction: build_phase										// 1 -> passed
  
   
  
@@ -92,33 +86,36 @@ class driver extends uvm_driver#(transaction);
 		vif.wr_enb <= '0;
 		vif.din <= '0;
 		vif.addr <= '0;
-      repeat(2) @(posedge vif.clk);							// wait for 2 clk cycles
-			`uvm_info(get_type_name(), "SYSTEM RESET", UVM_NONE)
+      repeat(5) @(posedge vif.clk);							// wait for 2 clk cycles
+      `uvm_info(get_type_name(), "---- SYSTEM RESET ----", UVM_NONE)
  
 		vif.rst <= '0;										// remove rst
  
-	endtask: reset_dut
- 
+	endtask: reset_dut 
+
+
+// ------------- drive task ------------
  
 	task drive();
  
-		@(posedge vif.clk);								// wait for one clk cycle
+		
 		vif.rst <= '0;									// remove rst
 		vif.wr_enb <= req.wr_enb;
 		vif.addr <= req.addr;
  
 		if(req.wr_enb) begin: B1						// if wr_enb is 1	
 			vif.din <= req.din;							// apply the i/p to dut
+          `uvm_info(get_type_name(), $sformatf(" DATA WRITE wr_data %0d", req.din), UVM_NONE)
  
-			repeat(2) @(posedge vif.clk);							// wait for 2 clk cycles
-			`uvm_info(get_type_name(), $sformatf(" DATA WRITE wr_data %0d", vif.din), UVM_NONE)	
+          	repeat(3) @(posedge vif.clk);				// wait for 3 clk cycles
 		end: B1
  
 		else begin: B2									// else means !vif.wr_enb
  
-			repeat(2) @(posedge vif.clk);						// wait for 2 clk cycles, to match wr and rd duration
-			req.dout = vif.dout;							// capture the stimulus from dut via vif
-			`uvm_info(get_type_name(), $sformatf(" DATA READ rd_data %0d", vif.dout), UVM_NONE)	
+			repeat(2) @(posedge vif.clk);				// wait for 2 clk cycles, to match wr and rd duration
+          `uvm_info(get_type_name(), $sformatf(" DATA READ rd_data %0d", vif.dout), UVM_NONE)	
+			 req.dout = vif.dout;						// capture the stimulus from dut via vif
+            @(posedge vif.clk);							 // wait for 1 clk cycles	
 		end: B2
 	endtask: drive
  
@@ -142,6 +139,80 @@ class driver extends uvm_driver#(transaction);
 
 endclass: driver
  
+      
+// ------------------- monitor class ---------------------
+ 
+ 
+// monitor class is not parameterize class.
+// component class in uvm base class hierarchy hence default constructor expect two arguments.
+// monitor responsibility is to capture the information from the DUT through vif and broadcast it to SB and RM(Multiple components) hence it has
+// analysis port.
+ 
+ 
+// monitor:- pin/signal level to pkt/stream of transactions level conversion.
+ 
+ 
+class monitor extends uvm_monitor;
+ 
+ 
+	transaction trans;									// instance for transaction class
+ 
+	virtual dut_interface vif;							// interface inside class need to declare as virtual
+														// bcz class is dynamic and interface is static component.
+	uvm_analysis_port #(transaction) ap;				// uvm_analysis_port, one to many kind of scenario			
+														// dimond shape and parameterize with type of xtn
+ 
+	`uvm_component_utils(monitor)						// factory registration
+ 
+	function new(input string name = "monitor", uvm_component parent = null);// component hence two arguments
+		super.new(name,parent);							// components are static in nature need to be there though out the simlation
+		ap = new("ap",this);							// memory allocation for ap, here or in build_phase anything is fine
+	endfunction: new
+ 
+	
+// build_phase:- responsible for building all lower level components
+//		 executes in top down manner.	
+ 
+ 
+	virtual function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		trans = transaction::type_id::create("trans");
+		if(!uvm_config_db #(virtual dut_interface)::get(this, "", "virtual_intf", vif))
+		`uvm_fatal(get_type_name(), "Unable to get virtual_intf vif")	
+	endfunction: build_phase	
+ 
+ 
+	task collect_data();
+      repeat(3) @(posedge vif.clk);						// wait 3 clk cycles, same delay as mention in drv
+				trans.wr_enb = vif.wr_enb;				// capturing the info form dut through vif hence blocking assignment
+				trans.addr   = vif.addr;		
+				trans.din    = vif.din;
+				trans.dout   = vif.dout;
+ 
+			
+				`uvm_info(get_type_name(), $sformatf(" DATA SEND TO SB wr_enb is %0d, addr is %0d, din is %0d, dout is %0d",
+								     trans.wr_enb, trans.addr, trans.din, trans.dout), UVM_NONE)
+				ap.write(trans);						// collecting data into ap
+														// ap has write method which is non-blocking in nature
+	endtask: collect_data
+ 
+ 
+// run_phase:- task bcz contain delay:- driver drive stimuls to DUT and monitor capture information from DUT based on DUT protocol.
+//	       executes in parallel and all pre-post run phases.
+ 
+ 
+	virtual task run_phase(uvm_phase phase);
+		forever begin: B1
+          collect_data();
+		end: B1
+	endtask: run_phase
+ 
+ 
+endclass: monitor
+ 
+ 
+      
+
  
 
 // ------------------- agent class  ----------------------
@@ -160,8 +231,10 @@ endclass: driver
 class agent extends uvm_agent;
  
    	`uvm_component_utils(agent)
-  	 driver drv;
-  	 uvm_sequencer#(transaction) seqr;
+  	 driver 						drv;
+  	 uvm_sequencer#(transaction) 	seqr;
+  	 monitor 						mon;
+
   	function new(input string name = "agent", uvm_component parent = null);
     		super.new(name, parent);
   	endfunction: new
@@ -174,6 +247,7 @@ class agent extends uvm_agent;
   	virtual function void build_phase(uvm_phase phase);
     		super.build_phase(phase);
     		drv = driver::type_id::create("drv", this);
+      		mon  = monitor::type_id::create("mon", this);
     		seqr = uvm_sequencer#(transaction)::type_id::create("seqr", this);
   	endfunction: build_phase
  
@@ -186,7 +260,83 @@ class agent extends uvm_agent;
   	endfunction: connect_phase
 
 endclass: agent
+
+
  
+      
+// ------------------- scoreboard class ---------------------
+ 
+ 
+// receive transactions from monitor and perform comparasion with golden data.
+// implementation details i.e write method(function) of analysis port.
+// component class in uvm base class hierarchy hence default constructor expect two arguments
+// no need to declare vif here
+ 
+      
+
+class scoreboard extends uvm_scoreboard;
+
+   uvm_analysis_imp #(transaction, scoreboard) aip;		// uvm_analysis_imp ,two arguments trans & class where implimentaion
+														// variable to store data, which we'r writing into reg during wr operation 
+	bit [7:0] temp_data;			
+ 
+	
+	`uvm_component_utils(scoreboard)					// factory regstration
+ 
+	function new(input string name = "scoreboard", uvm_component parent = null);	// component in UBCH hence two arguments	
+		super.new(name,parent);
+		aip = new("aip",this);							// object for aip
+	endfunction: new
+ 
+				
+ 
+ 
+// ap has write method which is non-blocking in nature i.e function
+ 
+ 
+	virtual function void write(input transaction tr);				// implementation details
+ 
+ 
+		`uvm_info(get_type_name(), $sformatf("data rcvd from Monitor ap, wr_enb is %0d, addr is %0d, din is %0d, dout is %0d",
+							tr.wr_enb, tr.addr, tr.din, tr.dout), UVM_NONE)
+ 
+		
+		if(tr.wr_enb) begin: B1 									// wr_enb == 1 i.e wr_operation
+			if(!tr.addr) begin: B2									// addr == 0
+				temp_data = tr.din;									// update the temp_data with din
+				`uvm_info(get_type_name(), $sformatf(" input data is stored %0d", tr.din) ,UVM_NONE)
+ 
+			end: B2
+ 
+			else begin: B3											// addr != 0
+ 
+				`uvm_info(get_type_name(), " ----  addr != 0 ---- ", UVM_NONE)
+			end: B3
+ 
+		end: B1
+ 
+		else  begin: B4				 								// wr_enb == 0 i.e rd_operation
+ 
+			if(!tr.addr) begin: B5									// addr == 0		
+              if(tr.dout ==  temp_data)							// data on o/p is equal to data in temp_data variable
+				`uvm_info(get_type_name()," ---- TEST IS PASSED ---- ",UVM_NONE)
+ 
+			end: B5
+ 
+			else begin: B6							// data on o/p is not equal to data in temp_data variable
+ 
+				`uvm_info(get_type_name()," ---- TEST IS FAILED ---- ",UVM_NONE)	
+			end: B6
+ 
+		end: B4
+ 
+	endfunction: write
+ 
+ 
+endclass: scoreboard
+ 
+ 
+      
  
  
 // ----------------------  register class  ----------------------
@@ -216,12 +366,12 @@ class reg0 extends uvm_reg;
 				.lsb_pos(0), 			// lsb postion
 				.access("RW"), 			// type of an access	
 				.volatile(0), 			// volatile -> 1 means field can't change between consecutive access		
-				.reset(0), 				// reset value(power on reset value for field)
+        	    .reset(0), 				// reset value(power on reset value for field)
 				.has_reset(1), 			// field support reset
 				.is_rand(1),			// field can be randomize
 				.individually_accessible(1)	// field is individually accessible
  
-				); 				// 9 arguments // psl avr hii
+				); 						// 9 arguments // psl avr hii
  
  
 	endfunction: build
@@ -258,9 +408,10 @@ class reg_blk extends uvm_reg_block;
 		default_map = create_map("default_map", 0, 1, UVM_LITTLE_ENDIAN);	// instance, base_address, size in byte(i.e 1*8 = 8), endian
 									// UVM_LITTLE_ENDIAN -> Least-significant bytes first in consecutive addresses
 		// adding register to map using add_reg method with 3 arguments
-		default_map.add_reg(reg0_h, 'h0, "RW");		// instance of register, offset_address, access, generally here "RW"
+ 
+		default_map.add_reg(reg0_h, 'h0, "RW");		// instance of register, offset_address, access, generally here "RW"  
 													// note:- base_address + offset_address = physical_address of reg0
-		default_map.set_auto_predict(1);			// implicit prediction, comment this and observe the result	
+        default_map.set_auto_predict(0);			// explicit  prediction, or comment this 	
 													// default_map -> bcz this method is related to map
 		lock_model();								// mandatory to lock the model
 													// Lock a model and build the address map.(it will build the map
@@ -282,60 +433,54 @@ class register_sequence extends uvm_sequence;
  
 	reg_blk  reg_blk_h;								// need to add instance of reg_block
  
- 
+	
 	function new(input string name = "register_sequence");
 		super.new(name);
 	endfunction: new
- 
- 
-	virtual task body();
- 
- 
+
+
+  	virtual task body();
+
+  		// type			field
 		uvm_status_e  	status;						// to store the status of txn
-        bit[7:0] 	    rdata_dv,rdata_mv;			// two varaibles same size of h/w reg
- 
- 
-		// ------ initial/default value in desired variable & Mirror variable-------
-		// note:- set and get methods work on desired and mirrored value of register model not on h/w register
- 
- 
-		rdata_dv = reg_blk_h.reg0_h.get();				// get the initial DV(get is a method before the txn)
-														// return a DV of the register model not the h/w register
- 
-		rdata_mv = reg_blk_h.reg0_h.get_mirrored_value();		// get the initial MV(get_mirrored_value is a method before the txn)
-														// return a MV of the register model not the h/w register
- 
-		`uvm_info(get_type_name(), $sformatf("initial Desired value is %0d, & Mirroed value is %0d", rdata_dv, rdata_mv), UVM_NONE)				
+		bit[7:0] 		dv_data, mv_data;			// two varaibles same size of h/w reg
+    	bit[7:0]		random_inp, read_out;		// variable to generate random inputs
 
- 
- 
-		// ------ set the desired variable -----
- 
-		reg_blk_h.reg0_h.set(8'd10);					// set the DV(set is a method before the txn)
-														// set a desired variable of the register model not the h/w register
-														// set method just set the desired variable of reg model
-														// mirrored value won't be affected with set method
-		// ------ get the desired & mirrored values in side of desired and mirrored variables -----
- 
-		rdata_dv = reg_blk_h.reg0_h.get();				// get the DV(get is a method before the txn)
-														// return a DV of the register model not the h/w register
- 
-		rdata_mv = reg_blk_h.reg0_h.get_mirrored_value();		// accessing the mirrored variable	
-		`uvm_info(get_type_name(), $sformatf("After set method Desired value is %0d, & Mirrored value is %0d", rdata_dv, rdata_mv), UVM_NONE)				
 
+      repeat(5) begin: B1
+		// ------ frontdoor write ------
+          random_inp = $urandom;
+
+          	reg_blk_h.reg0_h.write(status, random_inp, UVM_FRONTDOOR);
+			dv_data = reg_blk_h.reg0_h.get();						// get the desired variable
+ 
+			mv_data = reg_blk_h.reg0_h.get_mirrored_value();		// get the mirrored variable
+ 
+			`uvm_info(get_type_name(), $sformatf(" After frontdoor write method, Desired value is %0d, Mirroed value is %0d",
+						       dv_data, mv_data), UVM_NONE)
  
 		
-        reg_blk_h.reg0_h.write(status, 8'd5);				// writing 5 to the h/w reg 
-															// along with this it will update both desired & mirrored variables
-		rdata_dv = reg_blk_h.reg0_h.get();					// get the desired variable
- 
-		rdata_mv = reg_blk_h.reg0_h.get_mirrored_value();	// get the mirrored variable
- 
-		`uvm_info(get_type_name(), $sformatf(" After write method Desired value is %0d, & Mirrored value is %0d", rdata_dv, rdata_mv), UVM_NONE)				
 
  
+			reg_blk_h.reg0_h.read(status, read_out, UVM_FRONTDOOR);	
+
+			dv_data = reg_blk_h.reg0_h.get();						// get the desired variable, backdoor
+ 
+			mv_data = reg_blk_h.reg0_h.get_mirrored_value();		// get the mirrored variable, backdoor
+ 
+			`uvm_info(get_type_name(), $sformatf(" After frontdoor read method, Desired value is %0d, Mirroed value is %0d, read_out is %0d",
+						       dv_data, mv_data, read_out), UVM_NONE)	
+
+        $display(" ########################## -------------------- #################### ------------------ ######################### ");
+
+        end: B1
+ 
+ 
+      
+
 		// for register model to see the current state of h/w register(i.e mirrored) predictor is manadatory
-		// here there in no any predictor added, hence need to set_auto_predict(1);(i.e implicit prediction) in the reg_blk							
+        // here is explicit predictor we have added, hence need to set_auto_predict(0);(i.e disable implicit prediction) in the reg_blk	
+
  
 	endtask: body	
  
@@ -381,13 +526,14 @@ class adapter extends uvm_reg_adapter;
 		assert($cast(itm, bus_item));					// to get correct child class
  
 		rw.kind = (itm.wr_enb) ? UVM_WRITE : UVM_READ;			// updating the reg txn with bus txn
-		rw.data = itm.dout;
+		rw.data = (itm.wr_enb) ? itm.din : itm.dout;
 		rw.addr = itm.addr;
 		rw.status = UVM_IS_OK;
 	endfunction: bus2reg
  
  
 endclass: adapter
+ 
  
  
 // ----------------------  env class  ----------------------
@@ -404,9 +550,14 @@ endclass: adapter
 class env extends uvm_env;
  
  
-	agent 	 agt_h;
-	reg_blk  reg_blk_h;
-	adapter	 adapter_h;
+	agent 	 		agt_h;						// instance of an agent 
+	reg_blk 	 	reg_blk_h;					// instance of reg_blk
+	adapter	 		adapter_h;					// instance of an adapter
+
+	uvm_reg_predictor	#(transaction) predictor_h;			// instance of predictor(in build predictor)
+	scoreboard		    sb_h;
+
+ 
  
   	`uvm_component_utils(env)
 
@@ -421,7 +572,9 @@ class env extends uvm_env;
   
   	virtual function void build_phase(uvm_phase phase);
     		super.build_phase(phase);
+      		predictor_h = uvm_reg_predictor #(transaction)::type_id::create("predictor_h", this);
     		agt_h = agent::type_id::create("agt_h", this);
+      		sb_h = scoreboard::type_id::create("sb_h", this);
 			reg_blk_h = reg_blk::type_id::create("reg_blk_h", this);
 			reg_blk_h.build();						// calling the build method of reg_blk
 			adapter_h = adapter::type_id::create("adapter_h",, get_full_name());
@@ -437,11 +590,19 @@ class env extends uvm_env;
  
 	virtual function void connect_phase(uvm_phase phase);
 		super.connect_phase(phase);
-																					// map has instances for seqr and adapter
+ 
+		agt_h.mon.ap.connect(sb_h.aip);							// connection of mon(ap) and sb(aip)
+ 
 		reg_blk_h.default_map.set_sequencer(.sequencer(agt_h.seqr), .adapter(adapter_h)); // .set_sequencer method with 2 arguments
-		reg_blk_h.default_map.set_base_addr(0);						 					 // .set_base_addr method
-															  // at any point need to change base addr we can use this method
+		reg_blk_h.default_map.set_base_addr(0);						  // .set_base_addr method, base addr for map
+											 					 // at any point need to change base addr we can use this method		
+		predictor_h.map = reg_blk_h.default_map;				// predictor map with default_map which is reg_blk_h
+		predictor_h.adapter = adapter_h;						// accessing map and adapter handles
+ 
+		agt_h.mon.ap.connect(predictor_h.bus_in);								
+																	// connection of mon(ap) and predictor	
 	endfunction: connect_phase		
+
 endclass: env
  
  
@@ -600,3 +761,18 @@ module top_tb();
  
  
 endmodule: top_tb
+// interface 
+
+ 
+ 
+interface dut_interface();
+ 
+	parameter size = 8;
+	logic clk,rst;	  		 // active high rst
+	logic [size - 1 :0] din;  
+	logic wr_enb;		  	 // mode controll bit wr_enb = 1 then write in to ram else read 
+	logic addr;		   	
+	logic [size - 1 :0] dout;		   
+
+endinterface: dut_interface
+ 
